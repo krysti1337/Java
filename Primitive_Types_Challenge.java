a5c77e432255f08fe94a034ee150a3050152a661
Cristi <krystiberzedeanwork@gmail.com>
2024-05-14 16:27:03 +0300

    Primitive_Types_Challenge

--- Primitive_Types_Challenge.java
+++ Primitive_Types_Challenge.java
@@ -9,7 +9,15 @@ public class Primitive_Types_Challenge {
 
         long sum = (100000 + (int) (FirstIntVariable) + (short) (FirstShortVariable) + (byte) (FirstByteVariable));
 
+        //asa este gresit si nu se face in felul dat
         long FirstLongVariable = (long) sum;
         System.out.println("FirstLongVariable = "+ FirstLongVariable);
+
+        //metoda corecta
+        long sum2 = 100000L + 10L * (FirstByteVariable + FirstShortVariable + FirstIntVariable);
+
+        long SecondLongVariable = sum2;
+
+        System.out.println("The correct variant for this challenge "+ SecondLongVariable);
     }
 }
