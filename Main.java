22db04e29a40d025837a91e23b31961b3b2690cd
Cristi <krystiberzedeanwork@gmail.com>
2024-05-14 16:04:00 +0300

    casting in java with int and short

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
 
 public class Main {
     public static void main(String[] args) {
+
     short MyMinShortValue = Short.MIN_VALUE;
     int MyMinIntValue = Integer.MIN_VALUE;
+    byte MyMinByteValue = Byte.MIN_VALUE;
+
     System.out.println("Minimum short value: " + MyMinShortValue);
     System.out.println("Minimum int value: " + MyMinIntValue);
-    
-    byte MyMinByteValue = Byte.MIN_VALUE;
-    System.out.println("New byte value: " + MyMinByteValue);
-    byte MyNewByteValue = (byte) (MyMinByteValue / 2);
+    System.out.println("Minimum byte value: " + MyMinByteValue);
 
+    int MyNewIntValue =  MyMinIntValue / 2;
+    System.out.println("New int value: " + MyNewIntValue);
+
+    short MyNewShortValue = (short) (MyMinShortValue / 2);
+    System.out.println("New short value: " + MyNewShortValue);
+    
+    byte MyNewByteValue = (byte) (MyMinByteValue / 2);
     System.out.println("New short value: " + MyNewByteValue);
 
     }
