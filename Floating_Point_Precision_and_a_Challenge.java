13341011293adfe9eec6c3fdf57c2855f40578e9
Cristi <krystiberzedeanwork@gmail.com>
2024-05-16 16:21:21 +0300

    Floating_Point_Precision_and_a_Challenge

---
+++ Floating_Point_Precision_and_a_Challenge.java
@@ -0,0 +1,8 @@
+public class Floating_Point_Precision_and_a_Challenge {
+    public static void main(String[] args) {
+        double punds = 200d;
+        double kilo = punds * 0.45359237d;
+        System.out.println("kilo: " + kilo);
+
+    }
+}
