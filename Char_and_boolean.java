c4fed1ed754b6d0d1498a90382679e28fe48e680
Cristi <krystiberzedeanwork@gmail.com>
2024-05-16 16:45:28 +0300

    Char_and_boolean

---
+++ Char_and_boolean.java
@@ -0,0 +1,28 @@
+public class Char_and_boolean {
+    public static void main(String[] args){
+        char myChar = 'D';
+        
+        System.out.println("Char: " + myChar);
+
+        char myUnitcode = '\u0044';
+
+        System.out.println("unitcode: " + myUnitcode);
+        
+        char myDecimalCode = 68;
+
+        System.out.println("decimal code:" + myDecimalCode);
+
+        char Questionchar = '?';
+        char QuestionUnitCode = '\u003f';
+        char QuestionCharCode = 63;
+
+        System.out.println("question mark: " + Questionchar + " " + QuestionUnitCode + " " + QuestionCharCode);
+
+        boolean myTrueBooleanValue = true;
+        boolean MyFalseBooleanValue = false;
+
+        System.out.println("true boolean value: " + myTrueBooleanValue);
+        System.out.println("false boolean value: " + MyFalseBooleanValue);
+        
+    }
+}
