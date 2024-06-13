64a83ac03cd817d8e7b8c96c12991fdcf9150120
Cristi <krystiberzedeanwork@gmail.com>
2024-06-13 15:04:14 +0300

    Method Challenge

---
+++ Expressions_Statements_More/src/Method_Challenge.java
@@ -0,0 +1,28 @@
+public class Method_Challenge {
+    public static void main(String[] args) {
+        displayHighScorePosition("Tim", calculateHighScorePosition(1500));
+        displayHighScorePosition("Andrea", calculateHighScorePosition(1000));
+        displayHighScorePosition("George", calculateHighScorePosition(500));
+        displayHighScorePosition("Lucas", calculateHighScorePosition(100));
+        displayHighScorePosition("Nicolas", calculateHighScorePosition(25));
+
+
+    }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + " managed tot get into position " + position + " on the high score");
+
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if (playerScore >= 100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
