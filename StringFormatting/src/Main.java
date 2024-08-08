8f08c68355f9f557d9534fa31a0e37758190552b
Cristi <krystiberzedeanwork@gmail.com>
2024-08-08 12:07:23 +0300

    Operation with String

--- StringFormatting/src/Main.java
+++ StringFormatting/src/Main.java
@@ -32,5 +32,65 @@ public class Main {
 
         formattedString = "Your age is %d".formatted(age);
         System.out.println(formattedString);
+
+        printInformation("Hello World");
+        printInformation("");
+        printInformation("\t   \n");
+
+        String helloWorld = "Hello World";
+        System.out.printf("index of r = %d %n", helloWorld.indexOf('r')); //cautam r in Hello World
+        System.out.printf("index of World = %d %n", helloWorld.indexOf("World"));
+
+        System.out.printf("index of r = %d %n", helloWorld.indexOf('l')); //primul l din Hello Wrold
+        System.out.printf("index of r = %d %n", helloWorld.lastIndexOf('l')); //ultimul 'l' din Hello World
+
+        System.out.printf("index of r = %d %n", helloWorld.indexOf('l',3)); //cifra dupa virgula arata de unde trebuie sa caute in String index-ul
+        System.out.printf("index of r = %d %n", helloWorld.lastIndexOf('l',8));
+
+        String helloWorldLower = helloWorld.toLowerCase();
+
+        if(helloWorld.equals(helloWorldLower)){
+            System.out.println("Value match exactly");
+        }
+        if(helloWorld.equalsIgnoreCase(helloWorldLower)){
+            System.out.println("Value match ignoring case");
+        }
+
+        if(helloWorld.startsWith("Hello")){
+            System.out.println("String starts with Hello");
+        }
+
+        if(helloWorld.endsWith("World")){
+            System.out.println("String ends with World");
+        }
+
+        if(helloWorld.contains("World")){
+            System.out.println("String contains with World");
+        }
+
+        if(helloWorld.contentEquals("Hello World")){
+            System.out.println("Value match exactly");
+        }
+
+    }
+
+    public static void  printInformation(String string){
+
+        int length = string.length();
+
+        System.out.printf("Length = %d %n ",length);
+
+        if(string.isEmpty()){ //String nu are nimic in el
+            System.out.println("String is empty");
+            return;
+        }
+
+        if(string.isBlank()){ //String este gol dar are caracter ca de exemplu space
+            System.out.println("String is blank");
+        }
+        System.out.printf("First char = %c %n", string.charAt(0));
+
+        System.out.printf("Last char = %c %n", string.charAt(length-1));
+
     }
 }
