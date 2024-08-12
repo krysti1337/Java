748db7fd24944faeb16202adff4c30fd7c2cce52
Cristi <krystiberzedeanwork@gmail.com>
2024-08-12 15:37:16 +0300

    First composition Challenge

---
+++ Composition Challenge/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        SmartKitchen smartKitchen = new SmartKitchen();
+
+//        smartKitchen.getDishWasher().setHasWorkToDo(true);
+//        smartKitchen.getIceBox().setHasWorkToDo(true);
+//        smartKitchen.getBrewMaster().setHasWorkToDo(true);
+//
+//
+////        smartKitchen.getDishWasher().doDishes();
+////        smartKitchen.getIceBox().orderFood();
+////        smartKitchen.getBrewMaster().brewCoffee();
+
+        smartKitchen.setKitchenState(true, false, true);
+        smartKitchen.doKitchenWork();
+    }
+}
