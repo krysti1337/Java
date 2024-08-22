c7ef6ec6ca7a8fd519d350367272a8675975e595
Cristi <krystiberzedeanwork@gmail.com>
2024-08-22 22:14:46 +0300

    package

---
+++ Packages/src/dev/lpa/Main.java
@@ -0,0 +1,11 @@
+package dev.lpa;
+
+import com.abc.first.Item;
+
+public class Main {
+    public static void main(String[] args) {
+
+        com.abc.first.Item firstItem = new com.abc.first.Item("Burger"); //call item from   com.abc.first
+        System.out.println(firstItem);
+    }
+}
