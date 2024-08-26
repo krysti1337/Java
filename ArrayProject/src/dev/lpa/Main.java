48b4bc6320c9e0b6d64b4f1fe23eb52493559ef8
Cristi <krystiberzedeanwork@gmail.com>
2024-08-26 15:40:07 +0300

    Array part 2

--- ArrayProject/src/dev/lpa/Main.java
+++ ArrayProject/src/dev/lpa/Main.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -19,10 +21,37 @@ public class Main {
         System.out.println("Last = " + firstTen[arrayLength - 1]);
 
         int[] newArray;
-        newArray = new int[] {5, 4, 3, 2, 1}; //putem scrie si asa array
+//        newArray = new int[] {5, 4, 3, 2, 1}; //putem scrie si asa array
 
+        newArray = new int[5];
+        for(int i = 0; i < newArray.length; i++){ //putem valori in array
+            newArray[i] = newArray.length - i;
+        }
         for(int i = 0; i < newArray.length; i ++){
             System.out.print(newArray[i] +  " ");
         }
+
+        System.out.println();
+        //folosim for each
+        for (int element : newArray){
+            System.out.print(element + " ");
+        }
+
+        System.out.println(Arrays.toString(newArray));
+
+        Object objectVariable = newArray;
+
+        if(objectVariable instanceof int[]){ //In acest cod, atribui o referinta de matrice intreaga unei referinte de obiect
+            System.out.println("objectVariable is really an int array");
+        }
+
+        Object[] objectArray = new Object[3]; //putem crea o matrice de obiecte care accepta orice tip in Java
+
+        objectArray[0] = "Hello";
+        objectArray[1] = new StringBuilder("World");
+        objectArray[2] = Arrays.toString(newArray); //putem atribui un array catre unui alt array, daca scriem pur si simplu objectArray[2] = newArray, la System.out.print vom avea adresa lui newArray, nu si contentul lui
+
+        System.out.println(Arrays.toString(objectArray));
+
     }
 }
