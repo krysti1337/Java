4a65a404506482304bad31d8c51da3c5b63786f4
Cristi <krystiberzedeanwork@gmail.com>
2024-08-28 16:36:20 +0300

    First array challenge

---
+++ Arrays Challenge Exercise/src/dev/lpa/Main.java
@@ -0,0 +1,46 @@
+package dev.lpa;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = getRandomArray(10);
+
+        Arrays.sort(array);
+        reverse(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static int[] getRandomArray(int len) {
+
+        Random random = new Random();
+
+        int[] newInt = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            newInt[i] = random.nextInt(50);
+        }
+
+        return newInt;
+    }
+
+    public static void reverse(int[] array){
+        int n = array.length;
+
+        for(int i  = 0; i < n / 2; i ++){
+
+            // Stocarea temporară a elementelor din prima jumătate
+            int tem = array[i];
+
+            // Atribuirea primei reprize
+            // până la ultima jumătate
+            array[i] = array[n - i - 1];
+
+            // Atribuirea ultimei reprize
+            // la prima jumătate
+            array[n - i - 1] = tem;
+        }
+    }
+}
