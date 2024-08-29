a767fe20231971df219efaece2a25fa12ad8cad6
Cristi <krystiberzedeanwork@gmail.com>
2024-08-29 13:59:14 +0300

    Sorted Array Challenge

--- Sorted Array Challenge/src/SortedArray.java
+++ Sorted Array Challenge/src/SortedArray.java
@@ -3,26 +3,34 @@ import java.util.Scanner;
 
 public class SortedArray {
 
-    private int[] firstArray = ;
-
-    public int[] getIntegers(int len){
-
+    // Metoda pentru a obține un array de la utilizator
+    public static int[] getIntegers(int len) {
         int[] tempArray = new int[len];
         Scanner s = new Scanner(System.in);
-        boolean flag = true;
-            for(int i = 0; i < len; i++){
-                System.out.println("Enter element [" + tempArray[i] +"] :");
-                tempArray[i] = s.nextInt();
 
-            }
-        System.out.println("Array is :");
+        for (int i = 0; i < len; i++) {
+            System.out.println("Enter element [" + i + "] :");
+            tempArray[i] = s.nextInt();
+        }
         return tempArray;
     }
 
-    @Override
-    public String toString() {
-        return "SortedArray{" +
-                "firstArray=" + Arrays.toString(firstArray) +
-                '}';
+    // Metoda pentru a sorta array-ul în ordine descrescătoare
+    public static int[] sortIntegers(int[] array) {
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+        Arrays.sort(sortedArray);
+        for (int i = 0; i < sortedArray.length / 2; i++) {
+            int temp = sortedArray[i];
+            sortedArray[i] = sortedArray[sortedArray.length - 1 - i];
+            sortedArray[sortedArray.length - 1 - i] = temp;
+        }
+        return sortedArray;
+    }
+
+    // Metoda pentru a printa array-ul
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + " contents " + array[i]);
+        }
     }
 }
