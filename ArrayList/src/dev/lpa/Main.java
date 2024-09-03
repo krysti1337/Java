65feb345b736142474290b8eeeeb647fe41090b8
Cristi <krystiberzedeanwork@gmail.com>
2024-09-03 13:32:04 +0300

    Array List

---
+++ ArrayList/src/dev/lpa/Main.java
@@ -0,0 +1,34 @@
+package dev.lpa;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count) {
+
+    public GroceryItem(String name) {
+        this(name, "DAIRY", 1);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GroceryItem[] groceryArray = new GroceryItem[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+
+        System.out.println(Arrays.toString(groceryArray)); //[GroceryItem[name=milk, type=DAIRY, count=1], GroceryItem[name=apples, type=PRODUCE, count=6], GroceryItem[name=oranges, type=PRODUCE, count=5]]
+
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        
+
+    }
+}
