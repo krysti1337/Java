37e4c9a0f80f13e992812485c7179e191cfb76ae
Cristi <krystiberzedeanwork@gmail.com>
2024-09-05 12:47:13 +0300

    Array List

--- ArrayList/src/dev/lpa/MoreList/MoreList.java
+++ ArrayList/src/dev/lpa/MoreList/MoreList.java
@@ -1,6 +1,8 @@
 package dev.lpa.MoreList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class MoreList {
@@ -23,5 +25,59 @@ public class MoreList {
 
         groceries.addAll(nextList);
         System.out.println(groceries); //[apples, bananas, milk, yogurt, pickles, mustard, cheese]
+
+        System.out.println("Third item = " + groceries.get(2)); // Third item = milk
+
+        if(groceries.contains("mustard")){
+            System.out.println("List contains mustard"); //List contains mustard
+        }
+
+        groceries.add("yogurt");
+        System.out.println("first = " + groceries.indexOf("yogurt"));
+        System.out.println("last = " + groceries.lastIndexOf("yogurt"));
+//        first = 3
+//        last = 7
+
+        System.out.println(groceries);
+        groceries.remove(1); //sterge dupa index
+        System.out.println(groceries);
+        groceries.remove("yogurt"); //sterge dupa date
+        System.out.println(groceries);
+
+//        [apples, bananas, milk, yogurt, pickles, mustard, cheese, yogurt]
+//        [apples, milk, yogurt, pickles, mustard, cheese, yogurt]
+//        [apples, milk, pickles, mustard, cheese, yogurt]
+
+        groceries.removeAll(List.of("apples", "eggs")); //sterge toate elementele din lista cu asa nume
+        System.out.println(groceries);
+//        [milk, pickles, mustard, cheese, yogurt]
+
+        groceries.retainAll(List.of("apples","milk","mustard","cheese")); //sterge toate ca nu sunt aceste elemente si le lasa doar pe ele
+        System.out.println(groceries);
+//        [milk, mustard]
+
+        groceries.clear(); //sterge tot
+        System.out.println(groceries);
+        System.out.println("isEmpty = " + groceries.isEmpty());
+//        []
+//        isEmpty = true
+
+        groceries.addAll(List.of("apples", "milk", "mustard", "cheese"));
+        groceries.addAll(Arrays.asList("eggs", "pickles", "mustard", "ham"));
+
+        System.out.println(groceries);
+//        [apples, milk, mustard, cheese, eggs, pickles, mustard, ham]
+
+        groceries.sort(Comparator.naturalOrder());
+        System.out.println(groceries);
+//        [apples, cheese, eggs, ham, milk, mustard, mustard, pickles]
+
+        groceries.sort(Comparator.reverseOrder());
+        System.out.println(groceries);
+//        [pickles, mustard, mustard, milk, ham, eggs, cheese, apples]
+
+        var groceryArray = groceries.toArray(new String[groceries.size()]);
+        System.out.println(Arrays.toString(groceryArray));
+//        [pickles, mustard, mustard, milk, ham, eggs, cheese, apples]
     }
 }
