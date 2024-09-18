9ba6f0e891d7f003ab5208373f0ae269a45a29d5
Cristi <krystiberzedeanwork@gmail.com>
2024-09-18 16:12:26 +0300

    LinkedList Challenge final

--- Linked List Challenge/src/dev/lpa/Main.java
+++ Linked List Challenge/src/dev/lpa/Main.java
@@ -2,18 +2,62 @@ package dev.lpa;
 
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        LinkedList<CityAndDistance> city = new LinkedList<>();
 
+    private static ListIterator<CityAndDistance> iterator = null;
+
+    public static void main(String[] args) {
+        LinkedList<CityAndDistance> city = new LinkedList<>(); //creaza linkedList
+
+        //boolean variable
+        boolean flag = true;
+
+        //add list and check for dublicate
         addCity(city);
         checkForDublicate(city);
-        for (CityAndDistance town : city) {
-            town.getCityAndDistance();
-        }
+        iterator = city.listIterator(1);
 
-        goForward(city);
+        //Scanner
+        Scanner s = new Scanner(System.in);
+        printMenu();
+
+        while (flag) {
+            System.out.println("Introduceți o comandă:");
+            String input = s.nextLine().toUpperCase();
+
+            switch (input) {
+                case "F" -> {
+                    goForward(city);
+                }
+                case "B" -> {
+                    goBack(city);
+                }
+                case "L" -> {
+                    listPlaces(city);
+                }
+                case "Q" -> {
+                    flag = false;
+                }
+                default -> {
+                    System.out.println("Instructiune falsa");
+                break;
+                }
+
+            }
+        }
+    }
+
+    //print menu
+    public static void printMenu() {
+        System.out.println("""
+                Available actions (select word or letter) : 
+                 (F)orward
+                 (B)ackward
+                 (L)ist Places
+                 (M)enu
+                 (Q)uit""");
     }
 
     //add
@@ -30,23 +74,61 @@ public class Main {
     }
 
     //check for dublicate and if there are, remove from the list
+//    public static void checkForDublicate(LinkedList<CityAndDistance> city) {
+//        for (int i = 0; i < city.size(); i++) {
+//            for (int j = i + 1; j < city.size(); j++) {
+//                if (city.get(i).getCityName().equals(city.get(j).getCityName())) {
+//                    city.remove(j);
+//                }
+//            }
+//        }
+//    }
+    public static void listPlaces(LinkedList<CityAndDistance> city) {
+
+
+        ListIterator<CityAndDistance> iterator = city.listIterator(0);
+        System.out.println("From Sydney");
+        while (iterator.hasNext()){
+            CityAndDistance nextCity = iterator.next();
+            String nextCityName = nextCity.getCityName();
+            int nextDistance = nextCity.getDistance();
+            System.out.println("City - " + nextCityName + " are " + nextDistance + " km ");
+        }
+    }
+
     public static void checkForDublicate(LinkedList<CityAndDistance> city) {
-        for (int i = 0; i < city.size(); i++) {
-            for (int j = i + 1; j < city.size(); j++) {
-                if (city.get(i).getCityName().equals(city.get(j).getCityName())) {
-                    city.remove(j);
-                }
+        ListIterator<CityAndDistance> iterator = city.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().equals(iterator.next())) {
+                iterator.remove();
             }
         }
     }
 
-    public static CityAndDistance goForward(LinkedList<CityAndDistance> city) {
-        String previousTown = city.getFirst().getCityName();
-        ListIterator<CityAndDistance> iterator = city.listIterator(1);
-        var nextCityName = iterator.next().getCityName();
-        var nextDistance = iterator.next().getDistance();
-        CityAndDistance nextCity = new CityAndDistance(nextCityName, nextDistance);
-        System.out.println("Trip starts at -> " + city.getFirst().getCityName() + " Next City -> " + nextCityName + " distance " + nextDistance + " km");
-        return nextCity;
+    public static void goForward(LinkedList<CityAndDistance> city) {
+        String firstCity = city.getFirst().getCityName();
+
+        // Verificăm dacă iteratorul are următorul element
+        if (iterator != null && iterator.hasNext()) {
+            CityAndDistance nextCity = iterator.next();
+            String nextCityName = nextCity.getCityName();
+            int nextDistance = nextCity.getDistance();
+            System.out.println("From ---> " + firstCity + " to " + nextCityName + " are " + nextDistance + " km ");
+        } else {
+            System.out.println("Nu mai există orașe disponibile.");
+        }
+    }
+
+    public static void goBack(LinkedList<CityAndDistance> city) {
+        String firstCity = city.getFirst().getCityName();
+
+        if (iterator != null && iterator.hasPrevious()) {
+            CityAndDistance previousCity = iterator.previous();
+            String previousCityName = previousCity.getCityName();
+            int previousCityDistance = previousCity.getDistance();
+            System.out.println("From ---> " + firstCity + " to " + previousCityName + " are " + previousCityDistance + " km ");
+        } else {
+            System.out.println("Nu mai există orașe disponibile.");
+        }
     }
 }
